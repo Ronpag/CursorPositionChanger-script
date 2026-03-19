84a87822bae8b189206fb010d7a487902c7c8bcf
LarSen <sh0tnickobra@gmail.com>
2026-03-19 18:22:44 +0300

    V1

---
+++ CursorPositionChanger/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CursorPositionChanger;
+
+class Program
+{ 
+    [DllImport("user32.dll")]
+    static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+    
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int x, int y);
+    
+    [DllImport("user32.dll")]
+    static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
+    
+    const int WM_HOTKEY = 0x0312;
+    
+    const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+    const uint MOUSEEVENTF_LEFTUP = 0x0004;
+    
+    static void Main()
+    {
+        Console.WriteLine("Starting cursor position changer...");
+        RegisterHotKey(IntPtr.Zero, 1, 0,(uint)ConsoleKey.Tab); //u can rebind this key on another
+        
+        MSG msg;
+
+        while (GetMessage(out msg, IntPtr.Zero, 0, 0))
+        {
+            if (msg.message == WM_HOTKEY)
+            {
+                SetCursorPos(750, 243);
+                
+                Thread.Sleep(50);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, UIntPtr.Zero);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, UIntPtr.Zero);
+            }
+        }
+    }
+}
+
+public struct MSG
+{
+    public IntPtr hwnd;
+    public uint message;
+    public UIntPtr wParam;
+    public IntPtr lParam;
+    public uint time;
+}
